217c5fdc6e7f847cc0febb57fea5eb5f96542498
Orkun Kurul <orkunkurul@webinen.com>
2024-04-22 13:09:51 +0300

    LogBlock and Log services created

--- src/main/java/com/razortype/cyberproject/Repository/LogRepository.java
+++ src/main/java/com/razortype/cyberproject/Repository/LogRepository.java
@@ -1,9 +1,13 @@
 package com.razortype.cyberproject.Repository;
 
 import com.razortype.cyberproject.entity.Log;
+import com.razortype.cyberproject.entity.LogBlock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface LogRepository extends JpaRepository<Log, UUID> {
+    Page<Log> findByLogBlock(LogBlock logBlock, Pageable pageable);
 }
--- src/main/java/com/razortype/cyberproject/service/abstracts/LogService.java
+++ src/main/java/com/razortype/cyberproject/service/abstracts/LogService.java
@@ -1,4 +1,28 @@
 package com.razortype.cyberproject.service.abstracts;
 
+import com.razortype.cyberproject.api.dto.NewLogRequest;
+import com.razortype.cyberproject.api.dto.UpdateLogBlockRequest;
+import com.razortype.cyberproject.core.results.DataResult;
+import com.razortype.cyberproject.core.results.Result;
+import com.razortype.cyberproject.entity.Log;
+import com.razortype.cyberproject.entity.LogBlock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface LogService {
+
+    Result save(LogBlock logBlock);
+    Result save(Log log);
+    DataResult<List<LogBlock>> getAllLogBlock();
+    DataResult<LogBlock> getLogBlockById(UUID id);
+    Result updateLogBlock(UUID id, UpdateLogBlockRequest request);
+    Result saveNewLogToBlock(UUID logBlockId, NewLogRequest request);
+    DataResult<Page<Log>> getLogByLogBlock(UUID logBlockId, Pageable pageable);
+
+    DataResult<List<Log>> getAllLog();
+    DataResult<Log> getLogById(UUID id);
+
 }
